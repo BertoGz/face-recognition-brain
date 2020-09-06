3415ab2962b16fd3bad0b053037aa991e1a7d9aa
BertoGz <bertogzlx@gmail.com>
2020-09-06 03:08:25 -0700

    added Ranks component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import './App.css';
-import Navigation from './components/navigation/Navigation';
-import Logo from './components/logo/Logo'
-import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
+import React from "react";
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
 function App() {
   return (
     <div className="App">
-      <Navigation/>
-      <Logo/>
-      
-      <ImageLinkForm/>
-      {/*
+      <Navigation />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <Logo />
+        <Rank />
+      </div>
+      <ImageLinkForm />
+      {/*** 
       <FaceRecognition/>*/}
     </div>
   );
