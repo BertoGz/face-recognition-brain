f49b6f006b174a2302bc9e82df4c9445d9028380
bertogz <bertogzlx@gmail.com>
2021-11-25 13:26:42 -0800

    update styles

--- src/components/FaceRecognition.js
+++ src/components/FaceRecognition.js
@@ -6,42 +6,39 @@ const FaceRecognition = ({ boxValues, imgUrl }) => {
       style={{
         display: "flex",
         justifyContent: "center",
-        alignContent: "center",
       }}
     >
-      <div className="absolute mt2" style={{ maxWidth: "40%" }}>
-        <img id="aiImage" src={imgUrl} alt={""} />
-        {boxValues && (
-          <div
-            style={{
-              top: boxValues.topRow,
-              right: boxValues.rightCol,
-              bottom: boxValues.bottomRow,
-              left: boxValues.leftCol,
-              position: "absolute",
-              boxShadow: "0px 0px 0px 3px red",
-              display: "flex",
-              flexWrap: "wrap",
-              justifyContent: "center",
-              cursor: "pointer",
-            }}
-          >
-            <p> Face Detected</p>
-          </div>
-        )}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <div className="absolute mt2" style={{ maxWidth: "40%" }}>
+          <img id="aiImage" src={imgUrl} alt={""} />
+          {boxValues && (
+            <div
+              style={{
+                top: boxValues.topRow,
+                right: boxValues.rightCol,
+                bottom: boxValues.bottomRow,
+                left: boxValues.leftCol,
+                position: "absolute",
+                boxShadow: "0px 0px 0px 3px red",
+                display: "flex",
+                flexWrap: "wrap",
+                justifyContent: "center",
+                cursor: "pointer",
+              }}
+            >
+              <p> Face Detected</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
-};
-
-const styles = {
-  box: {
-    position: "absolute",
-    boxShadow: "3px",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    cursor: "pointer",
-  },
 };
 
 export default FaceRecognition;
