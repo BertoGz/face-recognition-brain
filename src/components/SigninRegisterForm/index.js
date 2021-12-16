b8c9f6b40cc4f5a25c6c17e06158bc3d894b614b
bertogz <bertogzlx@gmail.com>
2021-12-15 22:03:02 -0800

    add states to track email,pass

--- src/components/SigninRegisterForm/index.js
+++ src/components/SigninRegisterForm/index.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
+import { requestUserSignin } from "../../Requests";
 
 const SigninRegisterForm = ({ navigate }) => {
   const [isRegistering, setIsRegistering] = useState(false);
+  const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  console.log("hi", email, password);
+  const onSubmitSignin = async () => {
+    const response = await requestUserSignin({ email, password });
+    if (response.status > 0) {
+      navigate("home");
+    }
+  };
   return (
     <>
-      <article class="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m shadow-5 w-25-l mw6 center blur">
-        <main class=" black-80">
+      <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m shadow-5 w-25-l mw6 center blur">
+        <main className=" black-80">
           <h4
             style={{
               color: "white",
@@ -14,59 +26,72 @@ const SigninRegisterForm = ({ navigate }) => {
           >
             Face Recognition Brain
           </h4>
-          <form class="measure">
-            <fieldset id="sign_up" class="ba b--transparent ph0 mh0">
+          <form className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               {isRegistering && (
-                <div class="mt3">
-                  <label class="db fw6 lh-copy f6 white-80 " for="fullname">
+                <div className="mt3">
+                  <label
+                    className="db fw6 lh-copy f6 white-80 "
+                    htmlFor="fullname"
+                  >
                     Full Name
                   </label>
                   <input
-                    class="pa2 input-reset ba bg-transparent hover-white w-100"
+                    className="pa2 input-reset ba bg-transparent white hover-white w-100"
                     type="text"
                     name="fullname"
                     id="fullname"
                   />
                 </div>
               )}
-              <div class="mt3">
-                <label class="db fw6 lh-copy f6 white-80 " for="email-address">
+              <div className="mt3">
+                <label
+                  className="db fw6 lh-copy f6 white-80 "
+                  htmlFor="email-address"
+                >
                   Email
                 </label>
                 <input
-                  class="pa2 input-reset ba bg-transparent hover-white w-100"
+                  className="pa2 input-reset ba bg-transparent white hover-white w-100 "
                   type="email"
                   name="email-address"
                   id="email-address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div class="mv3">
-                <label class="db fw6 lh-copy f6 white-80" for="password">
+              <div className="mv3">
+                <label
+                  className="db fw6 lh-copy f6 white-80"
+                  htmlFor="password"
+                >
                   Password
                 </label>
                 <input
-                  class="b pa2 input-reset ba bg-transparent  hover-white w-100"
+                  className="b pa2 input-reset ba bg-transparent white hover-white w-100"
                   type="password"
                   name="password"
                   id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                 />
               </div>
 
               {!isRegistering && (
                 <div>
-                  <div class="mt3">
+                  <div className="mt3">
                     <input
                       style={{ backgroundColor: "rgb(0,100,100,.5)" }}
-                      class="b ph3 pv2 input-reset ba white-80 b--white grow pointer f6 dib"
+                      className="b ph3 pv2 input-reset ba white-80 b--white grow pointer f6 dib"
                       value="Sign in"
-                      onClick={() => navigate("home")}
+                      onClick={onSubmitSignin}
                     />
                   </div>
-                  <div class="lh-copy mt3">
+                  <div className="lh-copy mt3">
                     <input
                       style={{ backgroundColor: "rgb(100,100,100,.5)" }}
                       onClick={() => setIsRegistering(true)}
-                      class="b ph3 pv2 input-reset ba white-80 b--white  bg-green grow pointer f6 dib"
+                      className="b ph3 pv2 input-reset ba white-80 b--white  bg-green grow pointer f6 dib"
                       value="Sign-up"
                     />
                   </div>
@@ -74,20 +99,20 @@ const SigninRegisterForm = ({ navigate }) => {
               )}
               {isRegistering && (
                 <div>
-                  <div class="lh-copy mt3">
+                  <div className="lh-copy mt3">
                     <input
                       style={{ backgroundColor: "rgb(0,100,100,.5)" }}
                       onClick={() => navigate("home")}
-                      class="b ph3 pv2 input-reset ba white-80 b--white  bg-green grow pointer f6 dib"
+                      className="b ph3 pv2 input-reset ba white-80 b--white  bg-green grow pointer f6 dib"
                       value="Sign-up"
                     />
                   </div>
 
-                  <div class="lh-copy mt3">
+                  <div className="lh-copy mt3">
                     <input
                       style={{ backgroundColor: "rgb(100,100,0,.5)" }}
                       onClick={() => setIsRegistering(false)}
-                      class="b ph3 pv2 input-reset ba white-80 b--white  bg-green grow pointer f6 dib"
+                      className="b ph3 pv2 input-reset ba white-80 b--white  bg-green grow pointer f6 dib"
                       value="Already a user? Sign-in"
                     />
                   </div>
