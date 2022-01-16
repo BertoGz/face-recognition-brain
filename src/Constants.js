1089a6fa4b087f92376d443062a0025f15c1b71b
bertogz <bertogzlx@gmail.com>
2022-01-16 09:56:01 -0800

    update consts

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = "http://localhost:3000";
 export const GET_USERS_ENDPOINT = "/";
 export const SIGN_IN_ENDPOINT = "/signin";
+export const USER_REGISTER_ENDPOINT = "/register";
+export const INCREASE_ENTRY = "/image";
