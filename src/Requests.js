6003763d3a7a0ec0c17ec7580af434f5d3198ebc
bertogz <bertogzlx@gmail.com>
2022-01-16 09:55:49 -0800

    update requests

--- src/Requests.js
+++ src/Requests.js
@@ -1,9 +1,15 @@
-import { BASE_URL, GET_USERS_ENDPOINT, SIGN_IN_ENDPOINT } from "./Constants";
-const sendRequest = async ({ endpoint, body }) => {
+import {
+  BASE_URL,
+  GET_USERS_ENDPOINT,
+  INCREASE_ENTRY,
+  SIGN_IN_ENDPOINT,
+  USER_REGISTER_ENDPOINT,
+} from "./Constants";
+const sendRequest = async ({ endpoint, method, body }) => {
   const url = `${BASE_URL}${endpoint}`;
   console.log("url", url, endpoint);
   const res = fetch(url, {
-    method: "post",
+    method,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
   })
@@ -13,9 +19,9 @@ const sendRequest = async ({ endpoint, body }) => {
     });
   return res;
 };
-const postRequest = async ({ endpoint, body }) => {
+const postRequest = async ({ endpoint, method, body }) => {
   try {
-    const response = await sendRequest({ endpoint, body });
+    const response = await sendRequest({ endpoint, method, body });
     /* response
       .then((res) => res.json())
       .then((data) => console.log("hi!", data))
@@ -37,5 +43,27 @@ const postRequest = async ({ endpoint, body }) => {
 export const requestUserSignin = async ({ email, password }) =>
   postRequest({
     endpoint: SIGN_IN_ENDPOINT,
+    method: "post",
     body: { email, password },
+  });
+export const requestUserRegister = async ({
+  email,
+  confirmEmail,
+  password,
+  confirmPassword,
+  name,
+}) =>
+  postRequest({
+    endpoint: USER_REGISTER_ENDPOINT,
+    method: "post",
+    body: { email, confirmEmail, password, confirmPassword, name },
+  });
+
+export const increaseEntry = ({ id }) =>
+  postRequest({
+    endpoint: INCREASE_ENTRY,
+    method: "put",
+    body: {
+      id,
+    },
   });
