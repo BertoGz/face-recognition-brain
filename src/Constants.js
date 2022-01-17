d7dc2859c71e0e35c4f5e24f6c24c65c25bf9c2d
bertogz <bertogzlx@gmail.com>
2022-01-16 18:06:00 -0800

    update consts

--- src/Constants.js
+++ src/Constants.js
@@ -3,3 +3,4 @@ export const GET_USERS_ENDPOINT = "/";
 export const SIGN_IN_ENDPOINT = "/signin";
 export const USER_REGISTER_ENDPOINT = "/register";
 export const INCREASE_ENTRY = "/image";
+export const CLARIFAI = "/clarifai";
