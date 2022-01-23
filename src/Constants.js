253ba424d69a7226557d951c1617d5deddd1dca6
bertogz <bertogzlx@gmail.com>
2022-01-23 10:38:18 -0800

    update base url

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://afternoon-dusk-63055.herokuapp.com";
 export const GET_USERS_ENDPOINT = "/";
 export const SIGN_IN_ENDPOINT = "/signin";
 export const USER_REGISTER_ENDPOINT = "/register";
