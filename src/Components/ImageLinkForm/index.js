41f5143fc2f8d9307a2730cc95286d8c45afcae6
bertogz <bertogzlx@gmail.com>
2022-01-23 18:52:57 -0800

    add use of nativebase elements

--- src/Components/ImageLinkForm/index.js
+++ src/Components/ImageLinkForm/index.js
@@ -2,6 +2,18 @@ import React, { useState } from "react";
 import Logo from "../Logo";
 import "./ImageLinkForm.css";
 import Rank from "../Rank";
+import { Heading, Box, Button, HStack, Input } from "native-base";
+const inputStyles = {
+  minW: "100px",
+  flexGrow: 1,
+  placeholder: "Enter image URL",
+  placeholderTextColor: "white",
+
+  bgColor: "rgb(100,150,100)",
+  color: "white",
+  size: "xl",
+};
+
 const ImageLinkForm = ({ user, handleOnSubmit }) => {
   const [input, setInput] = useState("");
   const onInputChange = (e) => {
@@ -9,51 +21,22 @@ const ImageLinkForm = ({ user, handleOnSubmit }) => {
     setInput(e.target.value);
   };
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-      }}
-    >
-      <div
-        className="divv"
-        style={{ display: "flex", flexDirection: "column" }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-evenly",
-            paddingLeft: 250,
-          }}
-        >
-          <Logo />
-          <Rank {...{ user }} />
-        </div>
-        <p className="f3" style={{ fontFamily: "monospace" }}>
-          {
-            "This Magic Brain will detect faces in your pictures, give it a shot"
-          }
-        </p>
-        <div>
-          <div className="form center pa4 br3 shadow-1">
-            <input
-              className="f4 pa2 w-70 center"
-              type="text"
-              onChange={onInputChange}
-            />
-            <button
-              className="w-3 0 grow f4 link ph3 pv2 dib white bg-light-purple"
-              style={{ borderRadius: 10 }}
-              onClick={() => handleOnSubmit(input)}
-            >
-              Detect
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Box>
+      <Box alignItems={"flex-start"}>
+        <Rank {...{ user }} />
+      </Box>
+      <Logo />
+
+      <Heading size="md" color={"white"} py="3">
+        This Magic Brain will detect faces in your pictures, give it a shot
+      </Heading>
+      <HStack px="10%">
+        <Input {...inputStyles} onChange={onInputChange} />
+        <Button size="lg" onPress={() => handleOnSubmit(input)}>
+          Detect image
+        </Button>
+      </HStack>
+    </Box>
   );
 };
 
