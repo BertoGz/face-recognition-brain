8d6c306ebd626097fbb6d90372a4656d20b42778
bertogz <bertogzlx@gmail.com>
2022-01-23 17:59:46 -0800

    add home container

---
+++ src/Containers/Home/index.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import FaceRecognition from "../../Components/FaceRecognition";
+import ImageLinkForm from "../../Components/ImageLinkForm";
+import { clarifai, increaseEntry } from "../../Requests";
+
+const Home = (props) => {
+  const { user } = props || {};
+
+  const [imgUrl, setImgUrl] = useState("");
+  const [boxValues, setBoxValues] = useState(null);
+
+  const handleOnSubmit = async (input) => {
+    setImgUrl(input);
+    clarifai({ url: input })
+      .then(async (response) => {
+        if (response.data) {
+          const res = await increaseEntry({ id: user.current?.id });
+          if (res.status > 0) {
+            user.setState({ ...user.current, entries: res.data });
+            console.log("helo!");
+          }
+        }
+
+        calcFaceLocation(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const calcFaceLocation = (data) => {
+    console.log(data.outputs[0].data.regions[0].region_info.bounding_box);
+    let box = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("aiImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    setBoxValues({
+      leftCol: box.left_col * width,
+      topRow: box.top_row * height,
+      rightCol: width - box.right_col * width,
+      bottomRow: height - box.bottom_row * height,
+    });
+  };
+
+  return (
+    <>
+      <ImageLinkForm {...{ user }} handleOnSubmit={handleOnSubmit} />
+      <FaceRecognition boxValues={boxValues} imgUrl={imgUrl} />
+    </>
+  );
+};
+export default Home;
