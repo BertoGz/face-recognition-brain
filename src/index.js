b4e7e1e609a5a0fc4d71793bd7679428cf35f18d
bertogz <bertogzlx@gmail.com>
2022-01-23 18:31:47 -0800

    add nativebase wrapper

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'tachyons'
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "tachyons";
+import * as serviceWorker from "./serviceWorker";
+import { NativeBaseProvider } from "native-base";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <NativeBaseProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </NativeBaseProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
