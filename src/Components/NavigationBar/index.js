978c37521157faf0f77f6011ea8334f2ad0bed32
bertogz <bertogzlx@gmail.com>
2022-01-24 18:49:14 -0800

    add sign up option if guest user exists

--- src/Components/NavigationBar/index.js
+++ src/Components/NavigationBar/index.js
@@ -1,15 +1,22 @@
+import { Pressable, Text } from "native-base";
 import React from "react";
 
-const NavigationBar = ({ navigation, onSignOut }) => {
+const NavigationBar = ({ navigation, user, onSignOut }) => {
   return (
     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      {navigation.current !== "signin" && (
-        <p
-          onClick={onSignOut}
-          className="f4  link dim black underline pa3 pointer"
-        >
-          Sign out
-        </p>
+      {navigation.current !== "signin" && user?.current?.id && (
+        <Pressable onPress={onSignOut} p="5">
+          <Text fontSize={"xl"} color="white" underline>
+            Sign out
+          </Text>
+        </Pressable>
+      )}
+      {navigation.current !== "signin" && !user?.current?.id && (
+        <Pressable onPress={onSignOut} p="5">
+          <Text fontSize={"xl"} color="white" underline>
+            Sign up to keep your score!
+          </Text>
+        </Pressable>
       )}
     </nav>
   );
