56dfc8aa451e52f5c6cb842a403307e09cec3b2d
bertogz <bertogzlx@gmail.com>
2022-01-25 22:17:16 -0800

    remove unsed import

--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import FaceRecognition from "../../Components/FaceRecognition";
 import ImageLinkForm from "../../Components/ImageLinkForm";
 import { clarifai, increaseEntry } from "../../Requests";
