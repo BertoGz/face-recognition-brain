3075c7f8d5f9245d0efd2e8b09c24e7f2fc70de7
bertogz <bertogzlx@gmail.com>
2022-02-06 09:55:42 -0800

    update route prop to redux one

--- src/Components/NavigationBar/index.js
+++ src/Components/NavigationBar/index.js
@@ -1,17 +1,19 @@
 import { Pressable, Text } from "native-base";
 import React from "react";
+import { useSelector } from "react-redux";
 
-const NavigationBar = ({ navigation, user, onSignOut }) => {
+const NavigationBar = ({ route, onSignOut }) => {
+  const { user } = useSelector((state) => state);
   return (
     <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-      {navigation.current !== "signin" && user?.current?.id && (
+      {route !== "signin" && user?.id && (
         <Pressable onPress={onSignOut} p="5">
           <Text fontSize={"xl"} color="white" underline>
             Sign out
           </Text>
         </Pressable>
       )}
-      {navigation.current !== "signin" && !user?.current?.id && (
+      {route !== "signin" && !user?.id && (
         <Pressable onPress={onSignOut} p="5">
           <Text fontSize={"xl"} color="white" underline>
             Sign up to keep your score!
