f049eea70875ab928a12ac6682f0012ae582122c
bertogz <bertogzlx@gmail.com>
2022-02-06 10:15:05 -0800

    add configureStore helper

--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,1 +1,18 @@
-export * from "./Middleware";
+/* eslint-disable import/no-extraneous-dependencies */
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { rootReducer } from "./Reducers";
+
+function configureStore() {
+  const middleware = [];
+
+  middleware.push(thunk);
+  if (false) {
+    middleware.push(logger);
+  }
+
+  return createStore(rootReducer, applyMiddleware(...middleware));
+}
+
+export const store = configureStore();
