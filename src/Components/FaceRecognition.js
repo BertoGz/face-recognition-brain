a2254caef34caf9797711eceef7610ee3227aba1
bertogz <bertogzlx@gmail.com>
2022-02-06 11:02:15 -0800

    add use of native base comps

--- src/Components/FaceRecognition.js
+++ src/Components/FaceRecognition.js
@@ -1,43 +1,35 @@
+import { Center, Box, Text } from "native-base";
 import React from "react";
 
 const FaceRecognition = ({ boxValues, imgUrl }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignContent: "center",
-        }}
-      >
-        <div className="absolute mt2" style={{ maxWidth: "40%" }}>
-          <img id="aiImage" src={imgUrl} alt={""} />
-          {boxValues && (
-            <div
-              style={{
-                top: boxValues.topRow,
-                right: boxValues.rightCol,
-                bottom: boxValues.bottomRow,
-                left: boxValues.leftCol,
-                position: "absolute",
-                boxShadow: "0px 0px 0px 3px red",
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "center",
-                cursor: "pointer",
-              }}
-            >
-              <p> Face Detected</p>
-            </div>
-          )}
-        </div>
-      </div>
-    </div>
+    <Center>
+      <Center flex={1} display={"flex"}>
+        <img id="aiImage" src={imgUrl} alt={""} />
+        {boxValues && (
+          <Box
+            style={{
+              top: boxValues.topRow,
+              right: boxValues.rightCol,
+              bottom: boxValues.bottomRow,
+              left: boxValues.leftCol,
+              position: "absolute",
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              cursor: "pointer",
+            }}
+            bgColor={"rgba(255,255,255,.25)"}
+            borderRadius={"full"}
+          >
+            <Text color={"light.100"} fontWeight={"bold"}>
+              {" "}
+              Face Detected
+            </Text>
+          </Box>
+        )}
+      </Center>
+    </Center>
   );
 };
 
