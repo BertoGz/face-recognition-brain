12c153af83936e174f6daaf61aa58ef0c0b0489f
bertogz <bertogzlx@gmail.com>
2022-02-06 09:55:22 -0800

    add provider

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from "./App";
 import "tachyons";
 import * as serviceWorker from "./serviceWorker";
 import { NativeBaseProvider } from "native-base";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./Redux/Reducers";
+import { Middleware } from "./Redux";
+export const store = createStore(rootReducer, Middleware);
 
 ReactDOM.render(
   <NativeBaseProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
   </NativeBaseProvider>,
   document.getElementById("root")
 );
